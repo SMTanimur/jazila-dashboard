b9a74a14a9c439b170f2fb6eeae2c63f19f34afc
SMTanimur <smtanimurrahman404@gmail.com>
2024-02-24 11:13:10 +0600

    Remove useInternalRouter hook

--- src/hooks/use-internal-router.ts
+++
@@ -1,7 +0,0 @@
-import { RouterContext } from "@/shared/providers/router-provider"
-import { useContext } from "react"
-
-
-export const useInternalRouter = () => {
-  return useContext(RouterContext)
-}
